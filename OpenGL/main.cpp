68dbd02ad1d37203530b89afb4276529d3e180c0
Felipe <FelipeGutierrez@felipes-macbook-pro.local>
2018-03-23 08:03:46 -0500

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by Felipe on 3/23/18.
+//  Copyright © 2018 Felipe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
